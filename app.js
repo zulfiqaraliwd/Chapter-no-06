85e1ac8dc11c407f8706289e23d37fa2a5349631
Zulfiqar Ali <zulfiqaraliwd@gmail.com>
2024-09-15 15:31:28 +0500

    chapter no  completed

---
+++ app.js
@@ -0,0 +1,326 @@
+//Question no 01------------------------------------------------------------------
+/*
+var num = +prompt("enater any number to check");
+if (num % 3 ===0){
+    console.log("divisible by 3 ");
+}else{
+console.log("not divisible by 3");    
+}
+*/
+
+
+//Question no 02 ----------------------------------------------------------------------
+
+/*var num = +prompt("enter the number which have to be check");
+if(num % 2 === 0){
+    console.log("the number is even");
+}else{
+    console.log("the number is odd");
+}
+*/
+
+//Question no 03 ------------------------------------------------------------------------
+
+
+/*
+var age = prompt("enter your age");
+if(age > 18){
+    console.log("old enough")
+}else{
+    console.log("young too");
+}
+
+*/
+
+
+//Question no 04-------------------------------------------------------------------------
+/*
+var name = prompt("enter the name");
+if(name == "zulfiqar"){
+
+    console.log("Assalam o alikum dear ! How are you ? Hope you are well . How can i help you ?");
+}else{
+console.log("nothing here for you");
+}
+
+*/
+
+
+//Question no 05-----------------------------------------------------------------------------
+
+//switch case...repeated divisible by 3 wala
+
+
+//Question no 06-----------------------------------------------------------------------------
+
+/*
+var alphabete = +prompt("enter number");
+if(alphabete = 65){
+    console.log("Uppercase")
+}else if(alphabete = 97){
+console.log("Lowercase");
+}else{
+    console.log("invalid")
+}
+
+*/
+
+
+//Question no 07-------------------------------------------------------------------------
+/*
+var num1 = +prompt("enter the first number you wants to operate")
+var op = prompt("Enter the operation (+, -, *, /)");
+var num2 = prompt("enter the second number you wants to operate")
+var result;
+
+if (op === "+") {
+    result = num1 + num2;
+} else if (op === "-") {
+    result = num1 - num2;
+} else if (op === "*") {
+    result = num1 * num2;
+} else if (op === "/") {
+    result = num1 / num2;
+} else {
+    result = "Invalid operation try again";
+}
+
+console.log("Result: " + result);
+
+*/
+
+
+
+//Question no 08-----------------------------------------------------------------------------
+
+
+// let time=+prompt("enter time");
+// if(time>=0 && time<=1200){
+//     console.log("Good Morning");
+// }
+// else if(time>=1200 && time<=1700){
+//     console.log("Good After none")
+// }
+// else if(time>=1700 && time<=2100)
+// {
+//     console.log('good eveing')
+// }
+// else if(time>=2100 && time<=2350){
+//     console.log("Good Night");
+// }
+// else{
+//     console.log("not valid time")
+// }
+
+
+//Question no 09----------------------------------------------------------------------------
+
+//Leap year is that in which the day consist of 366 not 365.if leap year then it have should divisible by 400 or 4.
+
+/*var year = prompt("enter the year in form of YYYY");
+
+if (year % 4 == 0){
+
+    console.log("leap year")
+}else{
+    console.log("not leap year")
+}
+
+*/
+
+
+//Question no 10-----------------------------------------------------------------------------
+/*
+var pass = prompt("enter your password");
+
+if(pass == "smit123"){
+    console.log("Correct! the password you entered")
+}else if(pass == ""){
+    console.log("please enter your password")
+}else{
+    console.log("Incorrect password")
+}
+*/
+
+//Question no 11----------------------------------------------------------------------------
+/*
+var firstName = "Ali";
+if(firstName ==="Fahad"){
+    console.log("Hellow Ffahad");
+}else{
+    console.log("you are not fahad");
+}
+*/
+
+//Question no 12-----------------------------------------------------------------------------
+/*
+var greeting ;
+var hour = 19;
+if (hour < 18){
+    greeting = "Good day";
+}else{ 
+    greeting = "Good evening";
+}
+
+console.log(greeting);
+*/
+
+
+//Questio no 13----------------------------------------------------------------
+/*
+var num1 = prompt("enter the number-1");
+var num2 = prompt("enter the number-2");
+
+if(num1 < num2){
+console.log("greater number is " + num2);
+}else if(num2 < num1) {
+
+    console.log("greater number is " + num1)
+}
+
+*/
+
+
+//Question no 14----------------------------------------------------------------
+
+/*
+var number = prompt("enter the number");
+if(number == 0){
+    console.log("the number is zero")
+}else if (number < 0){
+    console.log("the number is negative ")
+}else if(number > 0){
+console.log("the number is possitive")
+}
+*/
+
+
+//Queation no 15-----------------------------------------------------------------
+/*
+var time = prompt("enter the time");
+if (time >= 6 && time <=9){
+    console.log("Breakfast is served")
+}else if(time >= 11 && time <= 13){
+    console.log("this time for lunch")
+}else if (time >= 17 && time <= 20){
+    console.log("Its dinner time.")
+}else{
+    console.log("sorry you will have t0 wait or go get a snack")
+}
+
+*/
+
+
+//Question no 16-----------------------------------------------------------------------
+/*
+
+i could not sole this question
+
+
+*/
+
+//Question no 17--------------------------------------------------------------------
+/*
+var alphabet = prompt("Enter the character");
+var a = "a";
+var e = "e";
+var i = "i";
+var o = "o";
+var u = "u";
+
+if (alphabet === a || alphabet === e || alphabet === i || alphabet === o || alphabet === u) {
+    console.log("This is a vowel");
+} else {
+    console.log("This is a consonant");
+}
+*/
+
+//Question no 18--------------------------------------------------------------------------
+
+/*
+var num1 = 10;
+var num2 = 8;
+if (num1 != num2){
+    console.log("true")
+}else{
+    console.log("false");
+}
+*/
+
+
+//Question no 19-----------------------------------------------------------------------------
+
+/*var month = prompt("enter the number to be check the month");
+
+if (month == 1){
+    console.log("january")
+
+
+}else if (month == 2){
+    console.log("faburary")
+
+    }else if (month == 3){
+        console.log("march")
+
+        
+
+    }else if (month == 4){
+        console.log("april")
+        
+        
+
+    }else if (month == 5){
+        console.log("may")
+        
+        
+
+    }else if (month == 6){
+        console.log("june")
+        
+        
+
+    }else if (month == 7){
+        console.log("july")
+        
+        
+
+    }else if (month == 8){
+        console.log("augest")
+        
+        
+
+    }else if (month == 9){
+        console.log("september")
+        
+        
+
+    }else if (month == 10){
+        console.log("octuber")
+        
+        
+
+    }else if (month == 11){
+        console.log("november")
+    
+
+}else if (month == 12){
+    console.log("december")
+
+}else{
+    console.log("invalid number of month")
+}
+*/
+
+
+//Question no 20------------------------------------------------------------------------
+
+
+/*
+var age = prompt("enter your age");
+if(age > 18){
+    console.log("old enough")
+}else{
+    console.log("young too");
+}
+
+*/
